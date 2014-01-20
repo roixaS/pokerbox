14006196c0e939f72bb0f65e35ac418f49f94353
roixa <ssbelow@mail.ru>
2014-01-20 15:45:19 +0700

    Create smallcardscene.h

---
+++ smallcardscene.h
@@ -0,0 +1,46 @@
+#ifndef SMALLCARDSCENE_H
+#define SMALLCARDSCENE_H
+
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsSceneMouseEvent>
+#include <QDebug>
+
+class SmallCardScene : public QGraphicsScene
+{
+    Q_OBJECT
+public:
+    explicit SmallCardScene(QObject *parent = 0);
+    float picSize;
+    QVector <QPoint> values;//ГЄГ Г°ГІГ» ГЁ Г¬Г Г±ГІГЁ
+    QVector <QPoint> choosedCards;//Г­ГҐГ«ГјГ§Гї Г¤ГўГ Г¦Г¤Г» ГўГ»ГЎГЁГ°Г ГІГј ГЄГ Г°ГІГі
+    void clearAll();
+    void setCardOnScene();
+    void baseClear();
+    float wight;
+    bool pushed;
+    bool changed;
+    QPoint push;
+    float hight;
+    void redraww(float,float);
+protected:
+
+void  mousePressEvent ( QGraphicsSceneMouseEvent  *eventt );
+bool event(QEvent *event);
+
+signals:
+    void choosen(QPoint );
+    void ckickedOnScene();
+
+
+
+public slots:
+    void removeCard(QPoint,bool);
+
+
+
+
+
+};
+
+#endif // SMALLCARDSCENE_H
